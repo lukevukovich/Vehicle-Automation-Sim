e58008f7679f7042679d084a4ea6d349e02733e7
lukevukovich <lukevuke83@gmail.com>
2022-12-05 17:14:38 -0500

    Function to add traffic to screen

--- Traffic/Traffic.cpp
+++ Traffic/Traffic.cpp
@@ -1,6 +1,14 @@
 #include "../Lane/Lane.h"
 #include "Traffic.h"
+#include "../TrafficLight/TrafficLight.h"
 
 void Traffic::setTraffic(char b[][WIDTH], int u, int p) {
-
+    int x;
+    if (u % 5 == 0) {
+        x = rand() % (GAP - 1) + p + 1;
+        if (b[x][WIDTH - 1] != STOP && b[x][WIDTH - 1] != GO) {
+            if (b[x][WIDTH - 2] != TRAFFIC)
+                b[x][WIDTH - 1] = TRAFFIC;
+        }
+    }
 }
--- Traffic/Traffic.h
+++ Traffic/Traffic.h
@@ -4,9 +4,15 @@
 #ifndef LANEAUTOMATIONSIM_TRAFFIC_H
 #define LANEAUTOMATIONSIM_TRAFFIC_H
 
+#define TRAFFIC '<'
+
+#define YELLOW "\x1B[33m"
+
 class Traffic {
+
 public:
     void setTraffic(char[][WIDTH], int, int);
+
 };
 
 #endif //LANEAUTOMATIONSIM_TRAFFIC_H
