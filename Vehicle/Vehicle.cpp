82534c418d6c456228aa7382c2e5ffbc2b512cb6
lukevukovich <lukevuke83@gmail.com>
2022-12-23 14:00:45 -0500

    Added reaction to oncoming traffic

--- Vehicle/Vehicle.cpp
+++ Vehicle/Vehicle.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "Vehicle.h"
 #include "../TrafficLight/TrafficLight.h"
+#include "../Traffic/Traffic.h"
 
 void Vehicle::setInitialVehicle(char b[][WIDTH], int x, int y) {
     b[x][y] = VEHICLE;
@@ -26,14 +28,44 @@ bool Vehicle::trafficLightLogic(char b[][WIDTH], int x, int y) {
     return r;
 }
 
-int Vehicle::automationLogic(char b[][WIDTH], int x, int y) {
-    int m = 0;
+int Vehicle::turnLogic(char b[][WIDTH], int x, int y) {
+    int m = 0, t;
     if (b[x][y + 1] == LANE) {
         turns++;
         if (b[x + 1][y] == LANE)
             m = -1;
         else if (b[x - 1][y] == LANE)
             m = 1;
+    }
+    else if (b[x][y + 1] == TRAFFIC) {
+        int lGap = 0, rGap = 0, i;
+
+        i = x;
+        while (b[i][y + 1] != LANE) {
+            lGap++;
+            i--;
+        }
+
+        i = x;
+        while (b[i][y + 1] != LANE) {
+            rGap++;
+            i++;
+        }
+
+        if (lGap > rGap)
+            m = -1;
+        else if (rGap > lGap)
+            m = 1;
+        else {
+            m = 1;
+
+            t = rand() % 2;
+            if (t == 0)
+                m = -m;
+        }
+
+        turns++;
+        traffic++;
     }
 
     return m;
@@ -58,6 +90,10 @@ void Vehicle::animateVehicle(char b[][WIDTH], int x, int y) {
 
 int Vehicle::getTurns() {
     return turns;
+}
+
+int Vehicle::getTraffic() {
+    return traffic;
 }
 
 int Vehicle::getLights() {
--- Vehicle/Vehicle.h
+++ Vehicle/Vehicle.h
@@ -11,9 +11,10 @@
 class Vehicle {
 private:
     //Keep track of sim data. Vehicle functions update these values
-    int units = 0;
+    int units = 1;
     int turns = 0;
     int lights = 0;
+    int traffic = 0;
 
 public:
 
@@ -24,13 +25,14 @@ public:
     bool trafficLightLogic(char[][WIDTH], int, int);
     //Get vehicle's vertical move (if needed) based on lane edges
     //Returns vertical move (-1, 0, 1)
-    int automationLogic(char[][WIDTH], int, int);
+    int turnLogic(char[][WIDTH], int, int);
     //Set vehicle's move
     void animateVehicle(char[][WIDTH], int, int);
 
     //Getter functions
     int getUnits();
     int getTurns();
+    int getTraffic();
     int getLights();
 
 };
