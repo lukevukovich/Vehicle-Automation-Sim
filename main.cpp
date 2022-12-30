f68b26cbaa17df65c21242abe7757858734bb63d
lukevukovich <lukevuke83@gmail.com>
2022-12-29 20:18:13 -0500

    Completed UI & ability to restart sim

--- main.cpp
+++ main.cpp
@@ -1,25 +1,14 @@
 #include <windows.h>
-#include <iostream>
 #include <ctime>
 #include <conio.h>
-#include <climits>
 #include "Lane/Lane.h"
 #include "Vehicle/Vehicle.h"
 #include "TrafficLight/TrafficLight.h"
 #include "Screen/Screen.h"
 #include "Traffic/Traffic.h"
+#include "InputUI/InputUI.h"
 
 using namespace std;
-
-int speedInput();
-int laneInput();
-int lightInput();
-int trafficInput();
-
-enum speed_enum {SLOW = 125, NORMAL = 25, FAST = 0};
-enum lane_enum {SMALL = 7,  MEDIUM = 10, LARGE = 13};
-enum light_enum {YES = 1, NO = 2};
-enum traffic_enum {NONE = 0, LIGHT = 8, HEAVY = 4};
 
 /*
  * All vehicle automation/detection logic is standalone from simulation generation.
@@ -36,236 +25,153 @@ int main() {
     int speedIn, lightIn, trafficIn, generate;
 
     time_t start, end;
-    int diff, elapsed = 0;
+    int diff, elapsed;
 
     bool isRedLight;
+    bool exit = false;
 
     srand(time(nullptr));
 
-    //Clear board/lane
-    for (i = 0; i < LENGTH; i++) {
-        for (j = 0; j < WIDTH; j++) {
-            board[i][j] = ' ';
-        }
-    }
-
     //Create objects
-    Screen screen = Screen();
-    Lane lane = Lane();
-    Vehicle vehicle = Vehicle();
-    TrafficLight trafficLight = TrafficLight();
-    Traffic oncomingTraffic = Traffic();
+    Screen screen;
+    Lane lane;
+    Vehicle vehicle;
+    TrafficLight trafficLight;
+    Traffic oncomingTraffic;
+    InputUI input;
 
-    speedIn = speedInput();
+    //Loop until user exits
+    while (!exit) {
 
-    gap = laneInput();
-    lane.setGap(gap);
-    screen.setLane(lane);
-    trafficLight.setLane(lane);
-    oncomingTraffic.setLane(lane);
+        //Reset objects
+        screen = Screen();
+        lane = Lane();
+        vehicle = Vehicle();
+        trafficLight = TrafficLight();
+        oncomingTraffic = Traffic();
+        input = InputUI();
 
-    lightIn = lightInput();
-
-    trafficIn = trafficInput();
-    oncomingTraffic.setInterval(trafficIn);
-
-    //Set initial lane position
-    //laneStart : starting y position of lane. Lane has width of GAP - 1
-    laneStart = rand() % (LENGTH - lane.getGap());
-    lane.setLanePiece(board, laneStart, WIDTH - 1);
-    lane.setInitialLane(board, laneStart);
-
-    //Set vehicle position values
-    vehicleX = laneStart + lane.getGap() - 1;
-    vehicleY = WIDTH/2;
-    vehicle.setInitialVehicle(board, vehicleX, vehicleY);
-
-    turns = 0, units = 0, lights = 0, traffic = 0;
-
-    //Start timer
-    time(&start);
-
-    //Loop until keyboard hit is detected
-    while(!kbhit()) {
-
-        //Print sim screen
-        screen.printSimScreen(board, laneStart, elapsed, units, turns, traffic, lights);
-
-        //Check for red light
-        isRedLight = vehicle.trafficLightLogic(board, vehicleX, vehicleY);
-
-        //If red light, stop for 2 seconds, turn light green
-        if (isRedLight) {
-            i = 0;
-            while (i < 5 && !kbhit()) {
-                if (i == 4) {
-                    trafficLight.turnGreen(board, vehicleY + 1);
-                }
-                //Update time while vehicle is stopped
-                time(&end);
-                diff = (int) difftime(end, start);
-                elapsed = diff;
-
-                //Update screen while vehicle is stopped
-                screen.printSimScreen(board, laneStart, elapsed, units, turns, traffic, lights);
-                Sleep(500);
-
-                i++;
+        //Clear board/lane
+        for (i = 0; i < LENGTH; i++) {
+            for (j = 0; j < WIDTH; j++) {
+                board[i][j] = ' ';
             }
         }
 
-        //Set number of lights passed
-        lights = vehicle.getLights();
+        //Get speed input from user
+        speedIn = input.speedInput();
 
-        //Get vehicle's vertical move based on lane position
-        vehicleMove = vehicle.turnLogic(board, vehicleX, vehicleY);
-        vehicleX += vehicleMove;
-        //Set number of vehicle turns and traffic passed
-        turns = vehicle.getTurns();
-        traffic = vehicle.getTraffic();
+        //Get lane width input from user
+        gap = input.laneInput();
+        //Set lane gap/width
+        lane.setGap(gap);
+        //Set lane object
+        //Used to obtain gap value within other objects
+        screen.setLane(lane);
+        trafficLight.setLane(lane);
+        oncomingTraffic.setLane(lane);
 
-        //Move all columns 1 to the left to give on animation effect
-        screen.animateLane(board);
-        //Get new chunk of lane to generate
-        laneStart = lane.getValidLanePiece(board, laneStart);
+        //Get traffic light input from user
+        lightIn = input.lightInput();
+
+        //Get traffic input from user
+        trafficIn = input.trafficInput();
+        oncomingTraffic.setInterval(trafficIn);
+
+        //Set initial lane position
+        //laneStart : starting y position of lane
+        laneStart = rand() % (LENGTH - lane.getGap() - 1);
         lane.setLanePiece(board, laneStart, WIDTH - 1);
-        //Set number of units traveled
-        units = vehicle.getUnits();
+        lane.setInitialLane(board, laneStart);
 
-        //Generate oncoming traffic
-        if (oncomingTraffic.getInterval() != 0)
-            oncomingTraffic.setTraffic(board, units, laneStart);
+        //Set vehicle position values
+        vehicleX = laneStart + lane.getGap() - 1;
+        vehicleY = WIDTH / 2;
+        vehicle.setInitialVehicle(board, vehicleX, vehicleY);
 
-        generate = rand() % 3;
-        //Generate traffic light
-        if (units > INTERVAL && generate == 0 && lightIn == YES)
-            trafficLight.setTrafficLight(board, units, laneStart);
+        turns = 0, units = 0, lights = 0, traffic = 0;
+        elapsed = 0;
 
-        //Move vehicle based on automation logic
-        vehicle.animateVehicle(board, vehicleX, vehicleY);
+        //Start timer
+        time(&start);
 
-        //Updated time
-        time(&end);
-        diff = (int) difftime(end, start);
-        elapsed = diff;
+        //Loop until keyboard hit is detected
+        while (!_kbhit()) {
 
-        Sleep(speedIn);
+            //Print sim screen
+            screen.printSimScreen(board, laneStart, elapsed, units, turns, traffic, lights);
+
+            //Check for red light
+            isRedLight = vehicle.trafficLightLogic(board, vehicleX, vehicleY);
+
+            //If red light, stop for 2 seconds, turn light green
+            if (isRedLight) {
+                i = 0;
+                while (i < 5 && !kbhit()) {
+                    if (i == 4) {
+                        trafficLight.turnGreen(board, vehicleY + 1);
+                    }
+                    //Update time while vehicle is stopped
+                    time(&end);
+                    diff = (int) difftime(end, start);
+                    elapsed = diff;
+
+                    //Update screen while vehicle is stopped
+                    screen.printSimScreen(board, laneStart, elapsed, units, turns, traffic, lights);
+                    Sleep(500);
+
+                    i++;
+                }
+            }
+
+            //Set number of lights passed
+            lights = vehicle.getLights();
+
+            //Get vehicle's vertical move based on lane position
+            vehicleMove = vehicle.turnLogic(board, vehicleX, vehicleY);
+            vehicleX += vehicleMove;
+            //Set number of vehicle turns and traffic passed
+            turns = vehicle.getTurns();
+            traffic = vehicle.getTraffic();
+
+            //Move all columns 1 to the left to give on animation effect
+            screen.animateLane(board);
+            //Get new chunk of lane to generate
+            laneStart = lane.getValidLanePiece(board, laneStart);
+            lane.setLanePiece(board, laneStart, WIDTH - 1);
+            //Set number of units traveled
+            units = vehicle.getUnits();
+
+            //Generate oncoming traffic
+            if (oncomingTraffic.getInterval() != 0)
+                oncomingTraffic.setTraffic(board, units, laneStart);
+
+            generate = rand() % 3;
+            //Generate traffic light
+            if (units > INTERVAL && generate == 0 && lightIn == input.getYESValue())
+                trafficLight.setTrafficLight(board, units, laneStart);
+
+            //Move vehicle based on automation logic
+            vehicle.animateVehicle(board, vehicleX, vehicleY);
+
+            //Updated time
+            time(&end);
+            diff = (int) difftime(end, start);
+            elapsed = diff;
+
+            //Used to control sim speed
+            Sleep(speedIn);
+
+        }
+
+        //If r is pressed, restart sim
+        if (_getch() != 'r')
+            exit = true;
+        //If any other key is pressed, exit sim
+        else
+            system("cls");
 
     }
 
     return 0;
-}
-
-int speedInput() {
-    int in;
-    speed_enum speed;
-
-    cout << " -----------\n |  SPEED  |\n -----------";
-    cout << "\n (1) Slow\n (2) Normal\n (3) Fast\n";
-    cout << "\n Select: ";
-    cin >> in;
-
-    while (!cin.good()) {
-        cin.clear();
-        cin.ignore(INT_MAX, '\n');
-
-        cout << " Select: ";
-        cin >> in;
-    }
-
-    if (in == 1)
-        speed = SLOW;
-    else if (in == 2)
-        speed = NORMAL;
-    else
-        speed = FAST;
-
-    system("cls");
-
-    return speed;
-}
-
-int laneInput() {
-    int in;
-    lane_enum lane;
-
-    cout << " ----------\n |  LANE  |\n ----------";
-    cout << "\n (1) Small\n (2) Medium\n (3) Large\n";
-    cout << "\n Select: ";
-    cin >> in;
-
-    while (!cin.good()) {
-        cin.clear();
-        cin.ignore(INT_MAX, '\n');
-
-        cout << " Select: ";
-        cin >> in;
-    }
-
-    if (in == 1)
-        lane = SMALL;
-    else if (in == 2)
-        lane = MEDIUM;
-    else
-        lane = LARGE;
-
-    system("cls");
-
-    return lane;
-}
-
-int lightInput() {
-    int in;
-    int light;
-
-    cout << " ------------------\n | TRAFFIC LIGHTS |\n ------------------";
-    cout << "\n (1) Yes\n (2) No\n";
-    cout << "\n Select: ";
-    cin >> in;
-
-    while (!cin.good()) {
-        cin.clear();
-        cin.ignore(INT_MAX, '\n');
-
-        cout << " Select: ";
-        cin >> in;
-    }
-
-    if (in == 1)
-        light = YES;
-    else
-        light = NO;
-
-    system("cls");
-
-    return light;
-}
-
-int trafficInput() {
-    int in;
-    traffic_enum traffic;
-
-    cout << " -----------\n | TRAFFIC |\n -----------";
-    cout << "\n (1) None\n (2) Light\n (3) Heavy\n";
-    cout << "\n Select: ";
-    cin >> in;
-
-    while (!cin.good()) {
-        cin.clear();
-        cin.ignore(INT_MAX, '\n');
-
-        cout << " Select: ";
-        cin >> in;
-    }
-
-    if (in == 1)
-        traffic = NONE;
-    else if (in == 2)
-        traffic = LIGHT;
-    else
-        traffic = HEAVY;
-
-    system("cls");
-
-    return traffic;
 }
